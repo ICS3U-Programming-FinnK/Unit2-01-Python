056440bbd78ba228820077606c3d8aa36366fca1
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-21 23:37:22 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Finn Kitor
+# Created on: September 21st, 2023
+# This program calculates and displays the area of a 
+# circle with radius of  7cm.
+import math
+
+def main():
+    print ("For a circle that has a radius")
+    print ("of 7.48 cm:")
+    print ("")
+    print ("Area = {} cm^2". format (math.pi*7.48**2))
+# lines 10-13 of code does the math for the area of a circle.
+
+if __name__ == "__main__":
+  main()
